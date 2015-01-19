46467c7deb0c7e14823323b311bb1f58969f63dc
Aendros <borxinhaa@gmail.com>
2015-01-19 19:13:56 +0100

    First commint on notes app

---
+++ notepad/www/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.button-begin').click(function(){
+    	$(".hello-app").remove();
+    }
+    	);
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
