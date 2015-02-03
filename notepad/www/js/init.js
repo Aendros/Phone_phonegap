36da9599ec1a0d1eb6e68e43e03127767ee47d06
Aendros <borxinhaa@gmail.com>
2015-02-03 20:36:00 +0100

    aded new changes
    Nothing to coment, just some atemps.

--- notepad/www/js/init.js
+++ notepad/www/js/init.js
@@ -1,12 +1,19 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.button-collapse').sideNav();
-    $('.button-begin').click(function(){
-    	$(".hello-app").remove();
-    	$("main").load("register.html");
-    }
-    	);
+        $('.button-collapse').sideNav();
+        $('.button-begin').click(function() {
+            $(".hello-app").remove();
+            $("main").load('ajax/register_login.html');
 
-  }); // end of document ready
+        });
+        $(document).on('click', '#reg_me', function() {
+            $("#registerOlog").remove();
+            $("main").load('ajax/registro.html');
+        });
+        $(document).on('click', '#log_me', function() {
+            alert('ok');
+        });
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
